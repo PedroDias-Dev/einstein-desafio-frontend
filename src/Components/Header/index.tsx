82de0aa05932f8ed2a6f906f0005e4a003d3f962
Pedro Dias <pedrofelipe18@hotmail.com.br>
2024-11-24 18:20:51 -0300

    chore: improve header layout

--- src/Components/Header/index.tsx
+++ src/Components/Header/index.tsx
@@ -1,7 +1,46 @@
+import { CircleUserRound, ShoppingCart } from "lucide-react";
 import * as S from "./styles";
 
+import Logo from "Assets/einstein-logo.jpg";
+
 const Header = () => {
-  return <S.Wrapper>Header</S.Wrapper>;
+  const links = [
+    { label: "Home", href: "/marketplace" },
+    { label: "Categorias", href: "/categories" },
+    { label: "Sobre", href: "/about" },
+    { label: "Contato", href: "/contact" },
+  ];
+
+  const isLinkActive = (link: string) => {
+    return window.location.pathname === link;
+  };
+
+  return (
+    <S.Wrapper>
+      <S.Logo>
+        <img src={Logo} alt="Logo" />
+        <h1>ECOMMERCE</h1>
+      </S.Logo>
+
+      <S.Container>
+        {links.map((link) => (
+          <S.Link
+            key={link.label}
+            href={link.href}
+            active={isLinkActive(link.href)}
+          >
+            {link.label}
+          </S.Link>
+        ))}
+      </S.Container>
+
+      <S.Container>
+        {/* // input */}
+        <ShoppingCart />
+        <CircleUserRound />
+      </S.Container>
+    </S.Wrapper>
+  );
 };
 
 export default Header;
--- src/Components/Header/styles.ts
+++ src/Components/Header/styles.ts
@@ -1,7 +1,45 @@
 import styled from "styled-components";
 
+interface LinkProps {
+  active: boolean;
+}
+
 export const Wrapper = styled.div`
   background-color: #fff;
-  padding: 20px;
-  width: 100%;
+  padding: 20px 50px;
+
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+export const Logo = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 10px;
+
+  img {
+    width: 100%;
+    height: 30px;
+    object-fit: contain;
+  }
+
+  h1 {
+    font-size: 18px;
+    font-weight: bold;
+  }
+`;
+
+export const Container = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 20px;
+`;
+
+export const Link = styled.a<LinkProps>`
+  color: #424242;
+  text-decoration: none;
+  font-size: 14px;
+  font-weight: ${(props: any) => (props.active ? "bold" : "normal")};
+  transition: 0.3s;
 `;
